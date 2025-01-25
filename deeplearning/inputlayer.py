e856c660ca0a2d3ca507b375c34563260fb0c937
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-01-24 21:49:56 -0500

    Create inputlayer.py

---
+++ deeplearning/inputlayer.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+
+
+def design_model(features):
+  model = Sequential(name = "my_first_model")
+  #get the number of features/dimensions in the data
+  num_features = features.shape[1] 
+  #without hard-coding
+  input = layers.InputLayer(input_shape=(num_features,)) 
+  #adding the input layer
+  model.add(input) 
+  return model
+
+
+dataset = pd.read_csv('insurance.csv') #load the dataset
+features = dataset.iloc[:,0:6] #choose first 7 columns as features
+labels = dataset.iloc[:,-1] #choose the final column for prediction
+
+features = pd.get_dummies(features) #one-hot encoding for categorical variables
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) #split the data into training and test data
+ 
+#standardize
+ct = ColumnTransformer([('standardize', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
+features_train = ct.fit_transform(features_train)
+features_test = ct.transform(features_test)
+
+#invoke the function for our model design
+model = design_model(features_train)
+print(model.summary())
