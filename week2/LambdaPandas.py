788799b3555fa7157dba851c64efffe8a7244e03
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-01-31 14:00:00 -0500

    Update LambdaPandas.py

--- week2/LambdaPandas.py
+++ week2/LambdaPandas.py
@@ -1,6 +1,23 @@
 import codecademylib3
 import pandas as pd
 
+# Lambda split
+get_last_name = lambda x: x.split()[-1]
+df['last_name'] = df.name.apply(get_last_name)
+
+print(df)
+
+# Using lambda for rows
+df = pd.read_csv('employees.csv')
+
+total_earned = lambda row: (row.hourly_wage * 40) + ((row.hourly_wage * 1.5) * (row.hours_worked - 40)) \
+	if row.hours_worked > 40 \
+  else row.hourly_wage * row.hours_worked
+  
+df['total_earned'] = df.apply(total_earned, axis = 1)
+
+print(df)
+
 # Load data
 orders = pd.read_csv('shoefly.csv')
 
