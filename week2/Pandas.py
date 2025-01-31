51c45ce9388c4078f9440d784c9af168ccee0747
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-01-31 13:38:00 -0500

    Update Pandas.py

--- week2/Pandas.py
+++ week2/Pandas.py
@@ -16,11 +16,19 @@ frances_palmer = orders[(orders.first_name == 'Frances') & (orders.last_name == 
 #Part 5: Comfy feet means more time on the street
 comfy_shoes = orders[orders.shoe_type.isin(['clogs', 'boots', 'ballet flats'])]
 
-# Cool lambda functionality
-df = pd.read_csv('employees.csv')
-
-# Add columns here
+# Lambda split
 get_last_name = lambda x: x.split()[-1]
 df['last_name'] = df.name.apply(get_last_name)
+
+print(df)
+
+# Using lambda for rows
+df = pd.read_csv('employees.csv')
+
+total_earned = lambda row: (row.hourly_wage * 40) + ((row.hourly_wage * 1.5) * (row.hours_worked - 40)) \
+	if row.hours_worked > 40 \
+  else row.hourly_wage * row.hours_worked
+  
+df['total_earned'] = df.apply(total_earned, axis = 1)
 
 print(df)
