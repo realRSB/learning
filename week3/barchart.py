8fefe52d325c3178eb2d92b8e9dd07301c246360
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-06 10:10:32 -0500

    Update barchart.py

--- week3/barchart.py
+++ week3/barchart.py
@@ -1,6 +1,9 @@
 ## YOUR SOLUTION HERE ##
-plt.bar(x=bar_data.PH, height=bar_data.average_leaf_width, width=0.8, align='center')
+plt.bar(x = bar_data.PH, height = bar_data.average_leaf_width, width = 0.8, align = 'center')
 plt.title('Effect of pH on Leaf Width')
 plt.xlabel('pH')
 plt.ylabel('Leaf Width (cm)')
+
+plt.errorbar(x = bar_data.PH, y = bar_data.average_leaf_width, yerr = bar_data.error, fmt = 'o', color = 'orangered')
+
 plt.show()
