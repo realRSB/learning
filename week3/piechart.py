23d53ef3dc0ced25565376d201d1effbc4bd997a
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-06 14:40:36 -0500

    Delete week3/piechart.py

--- week3/piechart.py
+++
@@ -1,5 +0,0 @@
-plt.pie(pie_data.number_sold, labels=pie_data.model, startangle=60, 
-        colors=['darkorange', 'gold', 'tomato', 'yellowgreen','darkmagenta',
-               'teal', 'indigo',  'powderblue', 'royalblue'])
-plt.title('Top 10 EVs sold in the US by count, 2021')
-plt.show()
