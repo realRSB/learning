e82a01f8a8bf33d7158622d6ea38e78cb77de35f
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-06 14:40:30 -0500

    Update Charts.py

--- week3/Charts.py
+++ week3/Charts.py
@@ -30,3 +30,10 @@ plt.legend(bbox_to_anchor = (1, .75))
 plt.show()
 
 plt.savefig('line_chart.png')
+
+## YOUR SOLUTION HERE ##
+plt.pie(pie_data.number_sold, labels=pie_data.model, startangle=60, 
+        colors=['darkorange', 'gold', 'tomato', 'yellowgreen','darkmagenta',
+               'teal', 'indigo',  'powderblue', 'royalblue'])
+plt.title('Top 10 EVs sold in the US by count, 2021')
+plt.show()
