63f191a1550afdb68fc71b7186aab937d0665c7a
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-06 14:41:04 -0500

    Delete week3/scatter.py

--- week3/scatter.py
+++
@@ -1,7 +0,0 @@
-## YOUR SOLUTION HERE ##
-plt.scatter(scatter_data.danceability, scatter_data.valence, color='teal', alpha=0.15)
-
-plt.title('Mood and Danceability correlation in Spotify genres')
-plt.xlabel('Danceability')
-plt.ylabel('Valence / Mood (sadder to happier)')
-plt.show()
