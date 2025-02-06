b3bda69099252332b7b9123a58cb4b4558f448da
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-05 21:48:19 -0500

    Create barchart.py

---
+++ week3/barchart.py
@@ -0,0 +1,6 @@
+## YOUR SOLUTION HERE ##
+plt.bar(x=bar_data.PH, height=bar_data.average_leaf_width, width=0.8, align='center')
+plt.title('Effect of pH on Leaf Width')
+plt.xlabel('pH')
+plt.ylabel('Leaf Width (cm)')
+plt.show()
