b12976eb666dd729e7c927753b465bb84fc760d0
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-06 10:36:12 -0500

    Create piechart.py

---
+++ week3/piechart.py
@@ -0,0 +1,5 @@
+plt.pie(pie_data.number_sold, labels=pie_data.model, startangle=60, 
+        colors=['darkorange', 'gold', 'tomato', 'yellowgreen','darkmagenta',
+               'teal', 'indigo',  'powderblue', 'royalblue'])
+plt.title('Top 10 EVs sold in the US by count, 2021')
+plt.show()
