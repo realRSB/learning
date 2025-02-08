c752900cfca607be79d6d173b41fab26a2f9fe02
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-08 12:01:02 -0500

    Update NPArrays.py

--- week3/NPArrays.py
+++ week3/NPArrays.py
@@ -23,3 +23,18 @@ thursday_friday_morning = temperatures_fixed[3:5,1]
 temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
 
 print(temperatures_fixed)
+
+import numpy as np
+
+cupcakes = np.array([2, 0.75, 2, 1, 0.5])
+
+recipes = np.genfromtxt('recipes.csv', delimiter=',')
+
+eggs = recipes[:,2]
+one_egg_recipes = eggs == 1
+
+cookies = recipes[0,:]
+
+double_batch = cupcakes * 2
+
+grocery_list = cookies + double_batch
