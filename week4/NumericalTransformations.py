adf8986c689fac4e3d0f031c1f5b9bf519c9640f
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-10 18:43:47 -0500

    Update NumericalTransformations.py

--- week4/NumericalTransformations.py
+++ week4/NumericalTransformations.py
@@ -1,4 +1,4 @@
-# Centering Data
+# 1. Centering Data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -24,3 +24,18 @@ print(centered_ages)
 
 plt.hist(centered_ages)
 plt.show()
+
+# 2. Standardizing Data
+import pandas as pd
+import numpy as np
+
+coffee = pd.read_csv('starbucks_customers.csv')
+ages = coffee['age']
+
+mean_age = np.mean(ages)
+std_dev_age = np.std(ages)
+
+ages_standardized = (ages - mean_age) / std_dev_age
+
+print(np.mean(ages_standardized))
+print(np.std(ages_standardized))
