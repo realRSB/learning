50be5a198f0ab714ee0bae4f9b3c899761f5f9d5
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-10 19:02:00 -0500

    Update NumericalTransformations.py

--- week4/NumericalTransformations.py
+++ week4/NumericalTransformations.py
@@ -89,3 +89,23 @@ reshaped_scaled = mmscaler.fit_transform(spent_reshaped)
 
 print(np.min(reshaped_scaled))
 print(np.max(reshaped_scaled))
+
+# 5. Binning our data
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import codecademylib3_seaborn 
+
+coffee = pd.read_csv('starbucks_customers.csv')
+ages = coffee['age']
+
+print(np.min(ages))
+print(np.max(ages))
+
+age_bins = [12, 20, 30, 40, 71]
+
+coffee['binned_ages'] = pd.cut(ages, age_bins, right = False)
+print(coffee['binned_ages'].head(10))
+
+coffee['binned_ages'].value_counts().plot(kind='bar')
+plt.show()
