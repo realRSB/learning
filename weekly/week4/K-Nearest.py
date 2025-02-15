e877aa7f9a0e02c9e84ac6bf43c21e93df1a424c
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-15 21:37:03 +0000

    created a regressor model

--- weekly/week4/K-Nearest.py
+++ weekly/week4/K-Nearest.py
@@ -1,3 +1,12 @@
+# KNN regressor model
+from movies import movie_dataset, movie_ratings
+from sklearn.neighbors import KNeighborsRegressor
+
+regressor = KNeighborsRegressor(n_neighbors = 5, weights = "distance")
+regressor.fit(movie_dataset,movie_ratings)
+
+print(regressor.predict([[0.016, 0.300, 1.022], [0.0004092981, 0.283, 1.0112], [0.00687649, 0.235, 1.0112]]))
+
 # using sk-learn
 from movies import movie_dataset, labels
 from sklearn.neighbors import KNeighborsClassifier
