1f69f8b05734b28c67ed40c23ac4cf5422fecc3a
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-14 19:10:38 -0500

    Update LogisticRegression.py

--- week4/LogisticRegression.py
+++ week4/LogisticRegression.py
@@ -1,23 +1,22 @@
-# Import libraries and data
+# Import pandas and the data
 import pandas as pd
-import numpy as np
-codecademyU = pd.read_csv('codecademyU.csv')
+codecademyU = pd.read_csv('codecademyU_2.csv')
 
-# Fit the logistic regression model
-hours_studied = codecademyU[['hours_studied']]
-passed_exam = codecademyU[['passed_exam']]
+# Separate out X and y
+X = codecademyU[['hours_studied', 'practice_test']]
+y = codecademyU.passed_exam
+
+# Transform X
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+# Split data into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 27)
+
+# Create and fit the logistic regression model here:
 from sklearn.linear_model import LogisticRegression
-model = LogisticRegression()
-model.fit(hours_studied,passed_exam)
-
-# Save intercept and coef
-intercept = model.intercept_
-coef = model.coef_
-
-# Calculate log_odds here
-log_odds = intercept + coef * hours_studied
-print(log_odds)
-
-# Calculate pred_probability_passing here
-pred_probability_passing = np.exp(log_odds)/(1+ np.exp(log_odds))
-print(pred_probability_passing)
+cc_lr = LogisticRegression()
+cc_lr.fit(X_train,y_train)
