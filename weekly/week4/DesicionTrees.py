065127532d43f65907b929401996a4ca76cead19
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-15 22:02:54 +0000

    Update DesicionTrees.py

--- weekly/week4/DesicionTrees.py
+++ weekly/week4/DesicionTrees.py
@@ -5,40 +5,33 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
-
-def gini(data):
-    """calculate the Gini Impurity
-    """
-    data = pd.Series(data)
-    return 1 - sum(data.value_counts(normalize=True)**2)
-   
-def info_gain(left, right, current_impurity):
-    """Information Gain associated with creating a node/split data.
-    Input: left, right are data in left branch, right banch, respectively
-    current_impurity is the data impurity before splitting into left, right branches
-    """
-    # weight for gini score of the left branch
-    w = float(len(left)) / (len(left) + len(right))
-    return current_impurity - w * gini(left) - (1 - w) * gini(right)
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'accep'])
 df['accep'] = ~(df['accep']=='unacc') #1 is acceptable, 0 if not acceptable
 X = pd.get_dummies(df.iloc[:,0:6])
 y = df['accep']
-
 x_train, x_test, y_train, y_test = train_test_split(X,y, random_state=0, test_size=0.2)
 
-y_train_sub = y_train[x_train['safety_low']==0]
-x_train_sub = x_train[x_train['safety_low']==0]
+## 1. Create a decision tree and print the parameters
+dtree = DecisionTreeClassifier()
+print(f'Decision Tree parameters: {dtree.get_params()}')
 
-gi = gini(y_train_sub)
-print(f'Gini impurity at root: {gi}')
+## 2. Fit decision tree on training set and print the depth of the tree
+dtree.fit(x_train, y_train)
+print(f'Decision tree depth: {dtree.get_depth()}')
 
-info_gain_list = []
-for i in x_train.columns:
-    left = y_train_sub[x_train_sub[i]==0]
-    right = y_train_sub[x_train_sub[i]==1]
-    info_gain_list.append([i, info_gain(left, right, gi)])
+## 3. Predict on test data and accuracy of model on test set
+y_pred = dtree.predict(x_test)
 
-info_gain_table = pd.DataFrame(info_gain_list).sort_values(1,ascending=False)
-print(f'Greatest impurity gain at:{info_gain_table.iloc[0,:]}')
+print(f'Test set accuracy: {dtree.score(x_test, y_test)}') # or accuracy_score(y_test, y_pred)
+
+## Visualizing the tree
+plt.figure(figsize=(27,12))
+tree.plot_tree(dtree, feature_names = x_train.columns,  
+            class_names = ['unacc', 'acc'],filled=True)
+plt.tight_layout()
+plt.show()
+
+## Text-based visualization of the tree (View this in the Output terminal!)
+print(tree.export_text(dtree, feature_names = x_train.columns.tolist()))
