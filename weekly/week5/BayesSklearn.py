657ed34eab29a72905cfc343cde7df35bbeaa4da
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-21 20:51:19 -0500

    Update BayesSklearn.py

--- weekly/week5/BayesSklearn.py
+++ weekly/week5/BayesSklearn.py
@@ -1,24 +1,26 @@
-from reviews import counter, training_counts
+from reviews import baby_counter, baby_training, instant_video_counter, instant_video_training, video_game_counter, video_game_training
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from reviews import neg_list, pos_list
-from sklearn.feature_extraction.text import CountVectorizer
 
-review = "This crib was amazing"
+review = "this game was violent"
 
-counter = CountVectorizer()
-counter.fit(neg_list + pos_list)
-print(counter.vocabulary_)
+baby_review_counts = baby_counter.transform([review])
+instant_video_review_counts = instant_video_counter.transform([review])
+video_game_review_counts = video_game_counter.transform([review])
 
-review_counts = counter.transform([review])
-print(review_counts.toarray())
+baby_classifier = MultinomialNB()
+instant_video_classifier = MultinomialNB()
+video_game_classifier = MultinomialNB()
 
-training_counts = counter.transform(neg_list + pos_list)
-review_counts = counter.transform([review])
+baby_labels = [0] * 1000 + [1] * 1000
+instant_video_labels = [0] * 1000 + [1] * 1000
+video_game_labels = [0] * 1000 + [1] * 1000
 
-classifier = MultinomialNB()
-training_labels = [0] * 1000 + [1] * 1000
-classifier.fit(training_counts, training_labels)
 
-print(classifier.predict(review_counts))
-print(classifier.predict_proba(review_counts))
+baby_classifier.fit(baby_training, baby_labels)
+instant_video_classifier.fit(instant_video_training, instant_video_labels)
+video_game_classifier.fit(video_game_training, video_game_labels)
+
+print("Baby training set: " +str(baby_classifier.predict_proba(baby_review_counts)))
+print("Amazon Instant Video training set: " + str(instant_video_classifier.predict_proba(instant_video_review_counts)))
+print("Video Games training set: " + str(video_game_classifier.predict_proba(video_game_review_counts)))
