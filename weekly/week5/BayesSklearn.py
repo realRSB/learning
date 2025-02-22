07e5c4ddfced8996d7e671d5bfe5c4d0da973b66
Rajveer Singh Bedi <167128438+realRSB@users.noreply.github.com>
2025-02-21 20:48:55 -0500

    Create BayesSklearn.py

---
+++ weekly/week5/BayesSklearn.py
@@ -0,0 +1,24 @@
+from reviews import counter, training_counts
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from reviews import neg_list, pos_list
+from sklearn.feature_extraction.text import CountVectorizer
+
+review = "This crib was amazing"
+
+counter = CountVectorizer()
+counter.fit(neg_list + pos_list)
+print(counter.vocabulary_)
+
+review_counts = counter.transform([review])
+print(review_counts.toarray())
+
+training_counts = counter.transform(neg_list + pos_list)
+review_counts = counter.transform([review])
+
+classifier = MultinomialNB()
+training_labels = [0] * 1000 + [1] * 1000
+classifier.fit(training_counts, training_labels)
+
+print(classifier.predict(review_counts))
+print(classifier.predict_proba(review_counts))
